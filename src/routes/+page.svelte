0aece740d955b7f2fb7b2708b6d8090bc9b74911
BahaaZidan <io@bahaazidan.com>
2026-04-08 15:11:59 +0200

    remove filter button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,4 @@
 <script lang="ts">
-	import FunnelIcon from '@lucide/svelte/icons/funnel';
-	import XIcon from '@lucide/svelte/icons/x';
-
-	import { resolve } from '$app/paths';
-
 	import Head from '$lib/components/Head.svelte';
 	import PostCard from '$lib/components/PostCard.svelte';
 	import { DEFAULT_DESCRIPTION, DEFAULT_TITLE } from '$lib/constants';
@@ -11,7 +6,6 @@
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
-	let dialog: HTMLDialogElement;
 </script>
 
 <Head
@@ -22,30 +16,6 @@
 />
 
 <main class="flex flex-col items-center">
-	<button class="btn min-w-48" onclick={() => dialog.showModal()}>
-		<FunnelIcon />
-		Filter
-	</button>
-	<dialog bind:this={dialog} class="modal">
-		<div class="modal-box h-144 w-88 md:w-xl">
-			<div class="flex flex-col items-end">
-				<form method="dialog">
-					<button class="btn btn-lg join-item rounded-full"><XIcon /></button>
-				</form>
-				<div class="flex max-w-7xl flex-wrap gap-2 px-4">
-					{#each data.tags as tag (tag)}
-						<a class="link-hover badge badge-secondary" href={resolve('/blog/tag/[tag]', { tag })}>
-							#{tag}
-						</a>
-					{/each}
-				</div>
-			</div>
-		</div>
-		<form method="dialog" class="modal-backdrop">
-			<button>close</button>
-		</form>
-	</dialog>
-
 	<section class="my-4 flex max-w-7xl flex-wrap justify-center gap-4">
 		{#each data.posts as post (post.id)}
 			<PostCard {post} />
